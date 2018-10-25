34a5e36ba81fd5eeff3e1beb14b145666758cbd8
bwixted <bill.wixted@quest.com>
2018-10-24 19:12:19 -0700

    k

--- index.php
+++ index.php
@@ -6,11 +6,13 @@
     $fileName = '/temp/crash'.date('Y-m-d_H-i-s').'.txt';
 
 
-    $file = fopen($fileName,'w') or die('Could not create report file: ' . $fileName);
-    foreach($_POST as $key => $value) {
-    $reportLine = $key." = ".$value."\n";
-        fwrite($file, $reportLine) or die ('Could not write to report file ' . $reportLine);
-    }
+    $file = fopen($fileName,'w') 
     fclose($file);
+
+    //foreach($_POST as $key => $value) {
+    //$reportLine = $key." = ".$value."\n";
+        //fwrite($file, $reportLine) or die ('Could not write to report file ' . $reportLine);
+    //}
+    //fclose($file);
 
 ?>
