1d5931998922863d7ba5aa8da37269002e1900b7
bwixted <bill.wixted@quest.com>
2018-10-25 15:11:00 -0700

    add sendgrid

--- index.php
+++ index.php
@@ -1,5 +1,24 @@
 <?php 
 
+require 'vendor/autoload.php';
+
+  $from = new SendGrid\Email(null, "test@example.com");
+  $subject = "Hello World from the SendGrid PHP Library!";
+  $to = new SendGrid\Email(null, "test@example.com");
+  $content = new SendGrid\Content("text/plain", "Hello, Email!");
+  $mail = new SendGrid\Mail($from, $subject, $to, $content);
+
+  $apiKey = getenv('SENDGRID_API_KEY');
+  $sg = new \SendGrid($apiKey);
+
+  $response = $sg->client->mail()->send()->post($mail);
+  echo $response->statusCode();
+  echo $response->headers();
+  echo $response->body();
+
+
+
+/**
   $fileName = 'logs.txt';
   //fileName = 'zzcrash_'.date('Y-m-d_H-i-s').'.txt';
 
@@ -22,5 +41,6 @@
 
   print("closing file");
   fclose($file);
+  **/
 
 ?>
