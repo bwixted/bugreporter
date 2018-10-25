d15dc3d7d12aebbc41e574d6f6cc2ce088e51d5a
bwixted <bill.wixted@quest.com>
2018-10-24 19:14:33 -0700

    k

--- index.php
+++ index.php
@@ -1,18 +1,14 @@
 <?php 
 
-  print "Hello World!"
-
-     Outputs all POST parameters to a text file. The file name is the date_time of the report reception
-    $fileName = '/temp/crash'.date('Y-m-d_H-i-s').'.txt';
+  $fileName = '/temp/crash'.date('Y-m-d_H-i-s').'.txt';
 
 
-    $file = fopen($fileName,'w') 
-    fclose($file);
+  $file = fopen($fileName,'w') 
 
-    //foreach($_POST as $key => $value) {
-    //$reportLine = $key." = ".$value."\n";
-        //fwrite($file, $reportLine) or die ('Could not write to report file ' . $reportLine);
-    //}
-    //fclose($file);
+  foreach($_POST as $key => $value) {
+    $reportLine = $key." = ".$value."\n";
+        fwrite($file, $reportLine) or die ('Could not write to report file ' . $reportLine);
+    }
+  fclose($file);
 
 ?>
